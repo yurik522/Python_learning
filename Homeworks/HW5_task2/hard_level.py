010f6e685b259041bf9b10198fcbcd61a0b117ed
Yurik <yurik522@gmail.com>
2022-07-04 12:10:02 +0300

    Home_work5_task2_architecture_continued

--- Homeworks/HW5_task2/hard_level.py
+++ Homeworks/HW5_task2/hard_level.py
@@ -1,16 +1,14 @@
 from random import randint
-from user_interface import parameters_game as param
 
 def first_move_user(total_count, max_amount):
-    
+   
     while total_count > 0:
-       
         user_count = int(input('Твой ход: \n'))
-        if  user_count <= 0:
-            print('Неправильный ход, нужно взять хотя бы одну конфету')
-            continue
         if user_count > max_amount:
             print('Неправильный ход, можно брать не более %d конфет' %(max_amount))
+            continue
+        if  user_count <= 0:
+            print('Нужно взять хотя бы одну конфету')
             continue
         total_count = total_count - user_count
         if total_count < 0:            
@@ -22,8 +20,12 @@ def first_move_user(total_count, max_amount):
             break
         else:
             print(f'осталось {total_count} конфет')
-        comp_count = randint(1, max_amount)
-        total_count = total_count - comp_count
+        if not total_count % (max_amount + 1): 
+            comp_count = randint(1, max_amount)
+            total_count = total_count - comp_count
+        else:
+            comp_count = total_count % (max_amount + 1)
+            total_count = total_count - comp_count
         if (total_count == 0) or (total_count < 0):
             print(f'Я забираю {total_count + comp_count} конфет \n Конфеты закончились)')                
             print('В этот раз победа за мной, ох и объемся же я сегодня (:')
@@ -34,14 +36,18 @@ def first_move_computer(total_count, max_amount):
     
     while total_count > 0:
        
-        comp_count = randint(1, max_amount)
-
-        total_count = total_count - comp_count
+        if not total_count % (max_amount + 1): 
+            comp_count = randint(1, max_amount)
+            total_count = total_count - comp_count
+        else:
+            comp_count = total_count % (max_amount + 1)
+            total_count = total_count - comp_count
         if (total_count == 0) or (total_count < 0):
             print(f'Я забираю {total_count + comp_count} конфет \n Конфеты закончились)')                
             print('В этот раз победа за мной, ох и объемся же я сегодня (:')
             break
         print(f'Я забираю {comp_count} конфет \n Осталось {total_count} конфет')
+
         user_count = int(input('Твой ход: \n'))
         while  user_count <= 0:
             print('Неправильный ход, нужно взять хотя бы одну конфету')
